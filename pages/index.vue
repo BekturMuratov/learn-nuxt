d1339cd8ce7210d3d1274b62febf0e14142967c1
BekturMuratov <Muratov225@gmail.com>
2021-12-22 17:27:46 +0600

    learn by doing

---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+<div>
+  <Nav/>
+  <div class="container">
+   
+<hero/>
+<div class="top"></div>
+<LargeCardDisplay v-for="cardInfo in largeCardInfo" :key="cardInfo.id" :cardSection="cardInfo"/>
+ <SmallCardDisplay 
+      v-for="cardInfo in smallCardSections"
+      :key="cardInfo.id"
+      :cardsSection="cardInfo"
+    /> 
+  </div>
+</div>
+</template>
+
+<script>
+import { largeCardSections, smallCardSections } from "@/assets/data.js"
+export default {
+  data(){
+    return {
+      largeCardInfo: largeCardSections,
+      smallCardSections: smallCardSections
+    }
+  }
+}
+</script>
+<style scoped>
+.top{
+  margin-top:20rem;
+}
+</style>
---
+++ pages/products/index.vue
@@ -0,0 +1,30 @@
+<template>
+<div>
+  <Nav/>
+  <div class="container">
+<LargeCardDisplay v-for="cardInfo in largeCardInfo.slice(0,1)" :key="cardInfo.id" :cardSection="cardInfo"/>
+ <SmallCardDisplay 
+      v-for="cardInfo in smallCardSections"
+      :key="cardInfo.id"
+      :cardsSection="cardInfo"
+    /> 
+  </div>
+</div>
+</template>
+
+<script>
+import { largeCardSections, smallCardSections } from "@/assets/data.js"
+export default {
+  data(){
+    return {
+      largeCardInfo: largeCardSections,
+      smallCardSections: smallCardSections
+    }
+  }
+}
+</script>
+<style scoped>
+.top{
+  margin-top:20rem;
+}
+</style>
